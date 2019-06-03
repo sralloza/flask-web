e1c8cae795893962f4f7c7f77a0d1b2a2b76f657
sralloza <sralloza@gmail.com>
2019-06-03 18:11:48 +0200

    Added link to origin of data in /new-menus

--- app/new_menus/routes.py
+++ app/new_menus/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, url_for
 
 from app.new_menus.motor import DailyMenusManager
+from app.utils import get_last_menus_page
 from . import new_menus
 
 
@@ -10,13 +11,16 @@ def new_menus_view():
     dmm = DailyMenusManager.load()
     for menu in dmm:
         menu.to_database()
-    return render_template('index.html', menus=dmm.menus[:15])
+
+    last_url = get_last_menus_page()
+    return render_template('index.html', menus=dmm.menus[:15], last_url=last_url)
 
 
 @new_menus.route('/n')
 @new_menus.route('/new_menus')
 def new_menus_redirect():
     return redirect('new-menus')
+
 
 @new_menus.route('/new-menus/reload')
 def new_menus_reload():
