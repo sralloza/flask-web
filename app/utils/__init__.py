ccf46c742ea2d29513816527a30b2e2239c4a501
sralloza <sralloza@gmail.com>
2019-06-23 11:17:54 +0200

    Added cache for this week's menus url

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,14 +1,20 @@
 import logging
+
 import requests
-
 from bs4 import BeautifulSoup as Soup
-
 
 logger = logging.getLogger(__name__)
 
 
+class _Static:
+    redirect_url = None
+
+
 def get_last_menus_page():
     principal_url = 'https://www.residenciasantiago.es/menus-1/'
+
+    if _Static.redirect_url:
+        return _Static.redirect_url
 
     try:
         response = requests.get(principal_url)
@@ -16,6 +22,7 @@ def get_last_menus_page():
         container = soup.findAll('div', {'class': 'j-blog-meta'})
 
         redirect_url = container[0].a['href']
+        _Static.redirect_url = redirect_url
         return redirect_url
     except Exception:
         logger.exception('Failed detecting this week\'s menus url')
