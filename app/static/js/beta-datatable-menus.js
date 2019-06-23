b191eed260ff56ba4bc17fa4fcf253d8d2ff1977
sralloza <sralloza@gmail.com>
2019-06-23 15:14:25 +0200

    Added js file with the datatable's code

---
+++ app/static/js/beta-datatable-menus.js
@@ -0,0 +1,15 @@
+$(document).ready( function () {
+        $('#menus_table').DataTable( {
+            // "ordering": false
+            "order": [[ 0, "desc" ]],
+            "columns": [
+            { "orderDataType": "date-dd-MMM-yyyy" },
+            null,
+            null,
+            null,
+            null
+            ]
+        });
+        console.log('loaded');
+    }
+);
