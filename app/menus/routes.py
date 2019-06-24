b1fb7f399a51f2dd2781581c5e70a9b6b7678803
sralloza <sralloza@gmail.com>
2019-06-24 13:36:30 +0200

    Created /hoy endpoint

--- app/menus/routes.py
+++ app/menus/routes.py
@@ -1,3 +1,6 @@
+import re
+from datetime import datetime, timedelta
+
 from flask import render_template, redirect, url_for, request
 
 from app.menus.motor import DailyMenusManager
@@ -39,3 +42,39 @@ def menus_redirect():
 @menus.route('/menus/reload')
 def menus_reload():
     return redirect(url_for('menus.menus_view', _external=True))
+
+
+@menus.route('/hoy')
+def today():
+    dmm = DailyMenusManager.load()
+    day = request.args.get('day')
+
+    asked = datetime.today()
+
+    if day in (None, ''):
+        menu = dmm[datetime.today().date()]
+    else:
+        asked = datetime.strptime(day, '%Y-%m-%d')
+        menu = dmm[asked.date()]
+
+    delta = timedelta(days=1)
+    tomorrow = asked + delta
+    yesterday = asked - delta
+
+    day = re.search(r'\((\w+)\)', menu.format_date()).group(1).capitalize()
+    day = f'{day} {menu.date.day}'
+
+    previous_url = None
+    next_url = None
+    if yesterday.date() in dmm:
+        previous_url = url_for('menus.today', _external=True) + '?day=' + str(yesterday.date())
+    if tomorrow.date() in dmm:
+        next_url = url_for('menus.today', _external=True) + '?day=' + str(tomorrow.date())
+
+    print(yesterday.date(), tomorrow.date())
+    print(previous_url, next_url)
+
+    return render_template(
+        'today.html', menu=menu, day=day, last_url=get_last_menus_page(),
+        previous_url=previous_url, next_url=next_url
+    )
