15b456c7487368197b39d372eab0195b8425442c
sralloza <sralloza@gmail.com>
2019-07-15 16:39:02 +0200

    Add test for menus.core.structure

--- app/tests/test_menus/test_structure.py
+++ app/tests/test_menus/test_structure.py
@@ -194,140 +194,179 @@ class TestMeal:
         assert m.p2 == 'p2'
 
 
-@pytest.mark.skip(reason='old')
+def gen_daily_menus():
+    lunch = (Meal(), Meal(p1='L1'), Meal(p1='L1', p2='L2'))
+    dinner = (Meal(), Meal(p1='D1'), Meal(p1='D1', p2='D2'))
+
+    product = list(itertools.product(lunch, dinner))
+    dates = [(x, 1, 2019) for x in range(1, len(product) + 1)]
+    return [DailyMenu(*x, *k) for x, k in zip(dates, product)]
+
+
 class TestDailyMenu:
+    esp_eng = (
+        ('12 febrero 2016 martes', '12 february 2016 tuesday'),
+        ('13 de diciembre de 2016 (martes)', '13 de december de 2016 (tuesday)'),
+        ('13 de diciembre de 2017 (miércoles)', '13 de december de 2017 (wednesday)'),
+        ('13 de enero de 2017 (viernes)', '13 de january de 2017 (friday)'),
+        ('14 de julio de 2017 (viernes)', '14 de july de 2017 (friday)'),
+        ('15 de agosto de 2017 (martes)', '15 de august de 2017 (tuesday)'),
+        ('16 de octubre de 2017 (lunes)', '16 de october de 2017 (monday)'),
+        ('16 enero viernes', '16 january friday'),
+        ('22 de septiembre de 2017 (viernes)', '22 de september de 2017 (friday)'),
+        ('23 de febrero de 2017 (jueves)', '23 de february de 2017 (thursday)'),
+        ('28 de junio de 2017 (miércoles)', '28 de june de 2017 (wednesday)'),
+        ('30 de mayo de 2017 (martes)', '30 de may de 2017 (tuesday)'),
+        ('6 de marzo de 2017 (lunes)', '6 de march de 2017 (monday)'),
+        ('7 de noviembre de 2017 (martes)', '7 de november de 2017 (tuesday)'),
+        ('9 de abril de 2017 (domingo)', '9 de april de 2017 (sunday)'),
+        ('día: 25 de febrero de 2019 (lunes)', 'día: 25 de february de 2019 (monday)'),
+    )
 
-    def test_e_to_s(self):
-        test = DailyMenu.e_to_s
-        assert test('12 february 2016 tuesday') == '12 febrero 2016 martes'
-        assert test('16 january friday') == '16 enero viernes'
-        assert test('16 january friday') == '16 enero viernes'
-        assert test('día: 25 de february de 2019 (monday)') == 'día: 25 de febrero de 2019 (lunes)'
-        assert test('13 de december de 2016 (tuesday)') == '13 de diciembre de 2016 (martes)'
-        assert test('13 de january de 2017 (friday)') == '13 de enero de 2017 (viernes)'
-        assert test('23 de february de 2017 (thursday)') == '23 de febrero de 2017 (jueves)'
-        assert test('6 de march de 2017 (monday)') == '6 de marzo de 2017 (lunes)'
-        assert test('9 de april de 2017 (sunday)') == '9 de abril de 2017 (domingo)'
-        assert test('30 de may de 2017 (tuesday)') == '30 de mayo de 2017 (martes)'
-        assert test('28 de june de 2017 (wednesday)') == '28 de junio de 2017 (miércoles)'
-        assert test('14 de july de 2017 (friday)') == '14 de julio de 2017 (viernes)'
-        assert test('15 de august de 2017 (tuesday)') == '15 de agosto de 2017 (martes)'
-        assert test('22 de september de 2017 (friday)') == '22 de septiembre de 2017 (viernes)'
-        assert test('16 de october de 2017 (monday)') == '16 de octubre de 2017 (lunes)'
-        assert test('7 de november de 2017 (tuesday)') == '7 de noviembre de 2017 (martes)'
-        assert test('13 de december de 2017 (wednesday)') == '13 de diciembre de 2017 (miércoles)'
+    datetimes = (
+        ('Día: 13 de diciembre de 2016 (martes)', datetime.date(2016, 12, 13)),
+        ('Día: 13 de enero de 2017 (viernes)', datetime.date(2017, 1, 13)),
+        ('Día: 23 de febrero de 2017 (jueves)', datetime.date(2017, 2, 23)),
+        ('Día: 6 de marzo de 2017 (lunes)', datetime.date(2017, 3, 6)),
+        ('Día: 9 de abril de 2017 (domingo)', datetime.date(2017, 4, 9)),
+        ('Día: 30 de mayo de 2017 (martes)', datetime.date(2017, 5, 30)),
+        ('Día: 28 de junio de 2017 (miércoles)', datetime.date(2017, 6, 28)),
+        ('Día: 14 de julio de 2017 (viernes)', datetime.date(2017, 7, 14)),
+        ('Día: 15 de agosto de 2017 (martes)', datetime.date(2017, 8, 15)),
+        ('Día: 22 de septiembre de 2017 (viernes)', datetime.date(2017, 9, 22)),
+        ('Día: 16 de octubre de 2017 (lunes)', datetime.date(2017, 10, 16)),
+        ('Día: 7 de noviembre de 2017 (martes)', datetime.date(2017, 11, 7)),
+        ('Día: 13 de diciembre de 2017 (miércoles)', datetime.date(2017, 12, 13)),
+        (datetime.date(2016, 12, 13), datetime.date(2016, 12, 13)),
+        (datetime.date(2017, 1, 13), datetime.date(2017, 1, 13)),
+        (datetime.date(2017, 2, 23), datetime.date(2017, 2, 23)),
+        (datetime.date(2017, 3, 6), datetime.date(2017, 3, 6)),
+        (datetime.date(2017, 4, 9), datetime.date(2017, 4, 9)),
+        (datetime.date(2017, 5, 30), datetime.date(2017, 5, 30)),
+        (datetime.date(2017, 6, 28), datetime.date(2017, 6, 28)),
+        (datetime.date(2017, 7, 14), datetime.date(2017, 7, 14)),
+        (datetime.date(2017, 8, 15), datetime.date(2017, 8, 15)),
+        (datetime.date(2017, 9, 22), datetime.date(2017, 9, 22)),
+        (datetime.date(2017, 10, 16), datetime.date(2017, 10, 16)),
+        (datetime.date(2017, 11, 7), datetime.date(2017, 11, 7)),
+        (datetime.date(2017, 12, 13), datetime.date(2017, 12, 13)),
+    )
 
-    def test_s_to_e(self):
-        test = DailyMenu.s_to_e
-        assert test('12 febrero 2016 martes') == '12 february 2016 tuesday'
-        assert test('16 enero viernes') == '16 january friday'
-        assert test('16 enero viernes') == '16 january friday'
-        assert test('día: 25 de febrero de 2019 (lunes)') == 'día: 25 de february de 2019 (monday)'
-        assert test('13 de diciembre de 2016 (martes)') == '13 de december de 2016 (tuesday)'
-        assert test('13 de enero de 2017 (viernes)') == '13 de january de 2017 (friday)'
-        assert test('23 de febrero de 2017 (jueves)') == '23 de february de 2017 (thursday)'
-        assert test('6 de marzo de 2017 (lunes)') == '6 de march de 2017 (monday)'
-        assert test('9 de abril de 2017 (domingo)') == '9 de april de 2017 (sunday)'
-        assert test('30 de mayo de 2017 (martes)') == '30 de may de 2017 (tuesday)'
-        assert test('28 de junio de 2017 (miércoles)') == '28 de june de 2017 (wednesday)'
-        assert test('14 de julio de 2017 (viernes)') == '14 de july de 2017 (friday)'
-        assert test('15 de agosto de 2017 (martes)') == '15 de august de 2017 (tuesday)'
-        assert test('22 de septiembre de 2017 (viernes)') == '22 de september de 2017 (friday)'
-        assert test('16 de octubre de 2017 (lunes)') == '16 de october de 2017 (monday)'
-        assert test('7 de noviembre de 2017 (martes)') == '7 de november de 2017 (tuesday)'
-        assert test('13 de diciembre de 2017 (miércoles)') == '13 de december de 2017 (wednesday)'
+    format_dates = (
+        (datetime.date(2016, 12, 13), '13 de diciembre de 2016 (martes)'),
+        (datetime.date(2017, 1, 13), '13 de enero de 2017 (viernes)'),
+        (datetime.date(2017, 2, 23), '23 de febrero de 2017 (jueves)'),
+        (datetime.date(2017, 3, 6), '06 de marzo de 2017 (lunes)'),
+        (datetime.date(2017, 4, 9), '09 de abril de 2017 (domingo)'),
+        (datetime.date(2017, 5, 30), '30 de mayo de 2017 (martes)'),
+        (datetime.date(2017, 6, 28), '28 de junio de 2017 (miércoles)'),
+        (datetime.date(2017, 7, 14), '14 de julio de 2017 (viernes)'),
+        (datetime.date(2017, 8, 15), '15 de agosto de 2017 (martes)'),
+        (datetime.date(2017, 9, 22), '22 de septiembre de 2017 (viernes)'),
+        (datetime.date(2017, 10, 16), '16 de octubre de 2017 (lunes)'),
+        (datetime.date(2017, 11, 7), '07 de noviembre de 2017 (martes)'),
+        (datetime.date(2017, 12, 13), '13 de diciembre de 2017 (miércoles)'),
+    )
 
-    # noinspection PyAttributeOutsideInit
-    @pytest.fixture
-    def init(self):
-        dm = DailyMenu
-        self.dm1 = dm(1, 1, 2019, lunch=Meal(), dinner=Meal())
-        self.dm2 = dm(2, 1, 2019, lunch=Meal(), dinner=Meal(p1='D1'))
-        self.dm3 = dm(4, 1, 2019, lunch=Meal(), dinner=Meal(p1='D1', p2='D2'))
-        self.dm4 = dm(5, 1, 2019, lunch=Meal(p1='L1'), dinner=Meal())
-        self.dm5 = dm(6, 1, 2019, lunch=Meal(p1='L1'), dinner=Meal(p1='D1'))
-        self.dm6 = dm(8, 1, 2019, lunch=Meal(p1='L1'), dinner=Meal(p1='D1', p2='D2'))
-        self.dm7 = dm(9, 1, 2019, lunch=Meal(p1='L1', p2='L2'), dinner=Meal())
-        self.dm8 = dm(9, 1, 2019, lunch=Meal(p1='L1', p2='L2'), dinner=Meal(p1='D1'))
-        self.dm9 = dm(9, 1, 2019, lunch=Meal(p1='L1', p2='L2'), dinner=Meal(p1='D1', p2='D2'))
+    _to_str = (
+        '1-martes-0-0', '2-miércoles-0-1', '3-jueves-0-2', '4-viernes-1-0',
+        '5-sábado-1-1',
+        '6-domingo-1-2', '7-lunes-2-0', '8-martes-2-1', '9-miércoles-2-2'
+    )
 
-        return True
+    @pytest.mark.parametrize('esp, eng', esp_eng)
+    def test_e_to_s(self, esp, eng):
+        assert DailyMenu.e_to_s(eng) == esp
 
-    def test_to_string(self, init):
-        assert init
+    @pytest.mark.parametrize('esp, eng', esp_eng)
+    def test_s_to_e(self, esp, eng):
+        assert DailyMenu.s_to_e(esp) == eng
 
-        assert self.dm1.to_string() == '01 de enero de 2019 (martes)\n'
-        assert self.dm2.to_string() == '02 de enero de 2019 (miércoles)\n - Cena\n   - D1\n'
-        assert self.dm3.to_string() == '04 de enero de 2019 (viernes)\n - Cena\n   - D1\n   - D2\n'
-        assert self.dm4.to_string() == '05 de enero de 2019 (sábado)\n - Comida\n   - L1\n'
-        assert self.dm5.to_string() == '06 de enero de 2019 (domingo)\n - Comida\n   - L1\n - Cena\n   - D1\n'
-        assert self.dm6.to_string() == '08 de enero de 2019 (martes)\n - Comida\n   - L1\n - Cena\n   - D1\n   - D2\n'
-        assert self.dm7.to_string() == '09 de enero de 2019 (miércoles)\n - Comida\n   - L1\n   - L2\n'
-        assert self.dm8.to_string() == '09 de enero de 2019 (miércoles)\n - Comida\n   - L1\n   - L2\n - Cena\n   - D1\n'
-        assert self.dm9.to_string() == '09 de enero de 2019 (miércoles)\n - Comida\n   - L1\n   - L2\n - Cena\n   - D1\n   - D2\n'
+    @pytest.mark.parametrize('dm, str_code', list(zip(gen_daily_menus(), _to_str)))
+    def test_to_string(self, dm, str_code):
+        # Code: day-weekday-lunch_code-dinner_code
+        # Meals codes: 0 -> emtpy, 1 -> P1, 2 -> P1 & P2
 
-    def test_to_html(self, init):
-        assert init
+        day, weekday, lunch_code, dinner_code = str_code.split('-')
 
-        assert self.dm1.to_html() == '01 de enero de 2019 (martes)<br>'
-        assert self.dm2.to_html() == '02 de enero de 2019 (miércoles)<br> - Cena<br>   - D1<br>'
-        assert self.dm3.to_html() == '04 de enero de 2019 (viernes)<br> - Cena<br>   - D1<br>   - D2<br>'
-        assert self.dm4.to_html() == '05 de enero de 2019 (sábado)<br> - Comida<br>   - L1<br>'
-        assert self.dm5.to_html() == '06 de enero de 2019 (domingo)<br> - Comida<br>   - L1<br> - Cena<br>   - D1<br>'
-        assert self.dm6.to_html() == '08 de enero de 2019 (martes)<br> - Comida<br>   - L1<br> - Cena<br>   - D1<br>   - D2<br>'
-        assert self.dm7.to_html() == '09 de enero de 2019 (miércoles)<br> - Comida<br>   - L1<br>   - L2<br>'
-        assert self.dm8.to_html() == '09 de enero de 2019 (miércoles)<br> - Comida<br>   - L1<br>   - L2<br> - Cena<br>   - D1<br>'
-        assert self.dm9.to_html() == '09 de enero de 2019 (miércoles)<br> - Comida<br>   - L1<br>   - L2<br> - Cena<br>   - D1<br>   - D2<br>'
+        day = int(day)
+        day_str = f'{day:02d} de enero de 2019 ({weekday})\n'
 
-    def test_from_datetime(self):
-        fdt = DailyMenu.from_datetime
+        lunch_code = int(lunch_code)
+        lunch_str = ''
+        if lunch_code != 0:
+            lunch_str += ' - Comida\n'
+            lunch_str += '   - L1\n'
+        if lunch_code == 2:
+            lunch_str += '   - L2\n'
 
-        assert fdt('Día: 13 de diciembre de 2016 (martes)').date == datetime.date(2016, 12, 13)
-        assert fdt('Día: 13 de enero de 2017 (viernes)').date == datetime.date(2017, 1, 13)
-        assert fdt('Día: 23 de febrero de 2017 (jueves)').date == datetime.date(2017, 2, 23)
-        assert fdt('Día: 6 de marzo de 2017 (lunes)').date == datetime.date(2017, 3, 6)
-        assert fdt('Día: 9 de abril de 2017 (domingo)').date == datetime.date(2017, 4, 9)
-        assert fdt('Día: 30 de mayo de 2017 (martes)').date == datetime.date(2017, 5, 30)
-        assert fdt('Día: 28 de junio de 2017 (miércoles)').date == datetime.date(2017, 6, 28)
-        assert fdt('Día: 14 de julio de 2017 (viernes)').date == datetime.date(2017, 7, 14)
-        assert fdt('Día: 15 de agosto de 2017 (martes)').date == datetime.date(2017, 8, 15)
-        assert fdt('Día: 22 de septiembre de 2017 (viernes)').date == datetime.date(2017, 9, 22)
-        assert fdt('Día: 16 de octubre de 2017 (lunes)').date == datetime.date(2017, 10, 16)
-        assert fdt('Día: 7 de noviembre de 2017 (martes)').date == datetime.date(2017, 11, 7)
-        assert fdt('Día: 13 de diciembre de 2017 (miércoles)').date == datetime.date(2017, 12, 13)
+        dinner_code = int(dinner_code)
+        dinner_str = ''
+        if dinner_code != 0:
+            dinner_str += ' - Cena\n'
+            dinner_str += '   - D1\n'
+        if dinner_code == 2:
+            dinner_str += '   - D2\n'
 
-        assert fdt(datetime.date(2016, 12, 13)).date == datetime.date(2016, 12, 13)
-        assert fdt(datetime.date(2017, 1, 13)).date == datetime.date(2017, 1, 13)
-        assert fdt(datetime.date(2017, 2, 23)).date == datetime.date(2017, 2, 23)
-        assert fdt(datetime.date(2017, 3, 6)).date == datetime.date(2017, 3, 6)
-        assert fdt(datetime.date(2017, 4, 9)).date == datetime.date(2017, 4, 9)
-        assert fdt(datetime.date(2017, 5, 30)).date == datetime.date(2017, 5, 30)
-        assert fdt(datetime.date(2017, 6, 28)).date == datetime.date(2017, 6, 28)
-        assert fdt(datetime.date(2017, 7, 14)).date == datetime.date(2017, 7, 14)
-        assert fdt(datetime.date(2017, 8, 15)).date == datetime.date(2017, 8, 15)
-        assert fdt(datetime.date(2017, 9, 22)).date == datetime.date(2017, 9, 22)
-        assert fdt(datetime.date(2017, 10, 16)).date == datetime.date(2017, 10, 16)
-        assert fdt(datetime.date(2017, 11, 7)).date == datetime.date(2017, 11, 7)
-        assert fdt(datetime.date(2017, 12, 13)).date == datetime.date(2017, 12, 13)
+        total_str = day_str + lunch_str + dinner_str
 
-    def test_format_date(self):
-        fdt = DailyMenu.from_datetime
+        assert dm.to_string() == total_str
 
-        assert fdt(datetime.date(2016, 12, 13)).format_date() == '13 de diciembre de 2016 (martes)'
-        assert fdt(datetime.date(2017, 1, 13)).format_date() == '13 de enero de 2017 (viernes)'
-        assert fdt(datetime.date(2017, 2, 23)).format_date() == '23 de febrero de 2017 (jueves)'
-        assert fdt(datetime.date(2017, 3, 6)).format_date() == '06 de marzo de 2017 (lunes)'
-        assert fdt(datetime.date(2017, 4, 9)).format_date() == '09 de abril de 2017 (domingo)'
-        assert fdt(datetime.date(2017, 5, 30)).format_date() == '30 de mayo de 2017 (martes)'
-        assert fdt(datetime.date(2017, 6, 28)).format_date() == '28 de junio de 2017 (miércoles)'
-        assert fdt(datetime.date(2017, 7, 14)).format_date() == '14 de julio de 2017 (viernes)'
-        assert fdt(datetime.date(2017, 8, 15)).format_date() == '15 de agosto de 2017 (martes)'
-        assert fdt(datetime.date(2017, 9, 22)).format_date() == '22 de septiembre de 2017 (viernes)'
-        assert fdt(datetime.date(2017, 10, 16)).format_date() == '16 de octubre de 2017 (lunes)'
-        assert fdt(datetime.date(2017, 11, 7)).format_date() == '07 de noviembre de 2017 (martes)'
-        assert fdt(
-            datetime.date(2017, 12, 13)).format_date() == '13 de diciembre de 2017 (miércoles)'
+    @pytest.mark.parametrize('dm, str_code', list(zip(gen_daily_menus(), _to_str)))
+    def test_to_html(self, dm, str_code):
+        # Code: day-weekday-lunch_code-dinner_code
+        # Meals codes: 0 -> emtpy, 1 -> P1, 2 -> P1 & P2
+
+        day, weekday, lunch_code, dinner_code = str_code.split('-')
+
+        day = int(day)
+        day_str = f'{day:02d} de enero de 2019 ({weekday})<br>'
+
+        lunch_code = int(lunch_code)
+        lunch_str = ''
+        if lunch_code != 0:
+            lunch_str += ' - Comida<br>'
+            lunch_str += '   - L1<br>'
+        if lunch_code == 2:
+            lunch_str += '   - L2<br>'
+
+        dinner_code = int(dinner_code)
+        dinner_str = ''
+        if dinner_code != 0:
+            dinner_str += ' - Cena<br>'
+            dinner_str += '   - D1<br>'
+        if dinner_code == 2:
+            dinner_str += '   - D2<br>'
+
+        total_str = day_str + lunch_str + dinner_str
+
+        assert dm.to_html() == total_str
+
+    @pytest.mark.parametrize('dt_to_parse, dt_expected', datetimes)
+    def test_from_datetime(self, dt_to_parse, dt_expected):
+        assert DailyMenu.from_datetime(dt_to_parse).date == dt_expected
+
+    @pytest.mark.parametrize('dt_to_parse, str_expected', format_dates)
+    def test_format_date(self, dt_to_parse, str_expected):
+        assert DailyMenu.from_datetime(dt_to_parse).format_date() == str_expected
 
     def test_update(self):
-        # todo make test
-        pass
+        dm = DailyMenu(1, 1, 2019)
+
+        lunch = Meal('L1', 'L2')
+        dinner = Meal('D1', 'D2')
+
+        dm.update(lunch=lunch, dinner=dinner)
+
+        assert dm.lunch == lunch
+        assert dm.dinner == dinner
+
+        with pytest.raises(ValueError, match='Lunch must be Meal'):
+            dm.update(lunch='peter')
+
+        with pytest.raises(ValueError, match='Dinner must be Meal'):
+            dm.update(dinner='pan')
+
+        dm.update(lunch1='lunch1', lunch2='lunch2',dinner1='dinner1',dinner2='dinner2')
+        assert dm.lunch == Meal('lunch1', 'lunch2')
+        assert dm.dinner == Meal('dinner1', 'dinner2')
