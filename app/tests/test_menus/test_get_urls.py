86a4d5eb6d9a52c880e634851ddf4f642dd1511e
sralloza <sralloza@gmail.com>
2019-07-18 15:30:14 +0200

    Test get_menus_urls

---
+++ app/tests/test_menus/test_get_urls.py
@@ -0,0 +1,120 @@
+from pathlib import Path
+from unittest import mock
+
+import pytest
+from requests.exceptions import ConnectionError
+
+from app.menus.core import get_menus_urls
+from app.menus.core.get_urls import PRINCIPAL_URL
+
+
+@mock.patch('requests.get')
+@mock.patch('app.menus.core.get_urls.logger')
+class TestGetMenusUrls:
+    urls_expected = ['https://www.residenciasantiago.es/2019/06/20/del-21-al-24-de-junio-2019/',
+                     'https://www.residenciasantiago.es/2019/06/17/del-18-al-20-de-junio-2019/']
+
+    warning_expected = 'Connection error downloading principal url (%r) (%d retries left)'
+
+    @pytest.fixture(scope='class')
+    def test_content(self):
+        path = Path(__file__).parent.parent / 'data' / 'get_urls.txt'
+
+        with path.open() as f:
+            return f.read()
+
+    def test_ok(self, logger_mock, requests_get_mock, test_content):
+        requests_get_mock.return_value.text = test_content
+        urls = get_menus_urls()
+
+        logger_mock.debug.assert_called_once_with('Getting menus urls')
+        assert len(urls) == 2
+        assert urls == self.urls_expected
+
+    def test_one_connection_error(self, logger_mock, requests_get_mock, test_content):
+        foo_mock = mock.Mock()
+        foo_mock.text = test_content
+        requests_get_mock.side_effect = iter([ConnectionError, foo_mock])
+        urls = get_menus_urls()
+
+        logger_mock.debug.assert_called_once_with('Getting menus urls')
+        logger_mock.warning.assert_has_calls([
+            mock.call(self.warning_expected, PRINCIPAL_URL, 4)
+        ])
+        assert len(urls) == 2
+        assert urls == self.urls_expected
+
+    def test_two_connection_error(self, logger_mock, requests_get_mock, test_content):
+        foo_mock = mock.Mock()
+        foo_mock.text = test_content
+        requests_get_mock.side_effect = iter([ConnectionError, ConnectionError, foo_mock])
+        urls = get_menus_urls()
+
+        logger_mock.debug.assert_called_once_with('Getting menus urls')
+        logger_mock.warning.assert_has_calls([
+            mock.call(self.warning_expected, PRINCIPAL_URL, 4),
+            mock.call(self.warning_expected, PRINCIPAL_URL, 3),
+        ])
+        assert logger_mock.warning.call_count == 2
+        assert len(urls) == 2
+        assert urls == self.urls_expected
+
+    def test_three_connection_error(self, logger_mock, requests_get_mock, test_content):
+        foo_mock = mock.Mock()
+        foo_mock.text = test_content
+        requests_get_mock.side_effect = iter(
+            [ConnectionError, ConnectionError, ConnectionError, foo_mock])
+        urls = get_menus_urls()
+
+        logger_mock.debug.assert_called_once_with('Getting menus urls')
+        logger_mock.warning.assert_has_calls([
+            mock.call(self.warning_expected, PRINCIPAL_URL, 4),
+            mock.call(self.warning_expected, PRINCIPAL_URL, 3),
+            mock.call(self.warning_expected, PRINCIPAL_URL, 2),
+        ])
+        assert logger_mock.warning.call_count == 3
+        assert len(urls) == 2
+        assert urls == self.urls_expected
+
+    def test_four_connection_error(self, logger_mock, requests_get_mock, test_content):
+        foo_mock = mock.Mock()
+        foo_mock.text = test_content
+        requests_get_mock.side_effect = iter(
+            [ConnectionError, ConnectionError, ConnectionError, ConnectionError, foo_mock])
+        urls = get_menus_urls()
+
+        logger_mock.debug.assert_called_once_with('Getting menus urls')
+        logger_mock.warning.assert_has_calls([
+            mock.call(self.warning_expected, PRINCIPAL_URL, 4),
+            mock.call(self.warning_expected, PRINCIPAL_URL, 3),
+            mock.call(self.warning_expected, PRINCIPAL_URL, 2),
+            mock.call(self.warning_expected, PRINCIPAL_URL, 1),
+        ])
+        assert logger_mock.warning.call_count == 4
+        assert len(urls) == 2
+        assert urls == self.urls_expected
+
+    def test_five_connection_error(self, logger_mock, requests_get_mock, test_content):
+        foo_mock = mock.Mock()
+        foo_mock.text = test_content
+        requests_get_mock.side_effect = iter(
+            [ConnectionError, ConnectionError, ConnectionError, ConnectionError, ConnectionError,
+             foo_mock])
+        urls = get_menus_urls()
+
+        logger_mock.debug.assert_called_once_with('Getting menus urls')
+        logger_mock.warning.assert_has_calls([
+            mock.call(self.warning_expected, PRINCIPAL_URL, 4),
+            mock.call(self.warning_expected, PRINCIPAL_URL, 3),
+            mock.call(self.warning_expected, PRINCIPAL_URL, 2),
+            mock.call(self.warning_expected, PRINCIPAL_URL, 1),
+            mock.call(self.warning_expected, PRINCIPAL_URL, 0),
+        ])
+        assert logger_mock.warning.call_count == 5
+
+        logger_mock.critical.assert_called_once_with(
+            'Fatal connection error downloading principal url (%r). No urls found (%d retries)',
+            PRINCIPAL_URL, 5)
+
+        assert len(urls) == 0
+        assert urls == []
