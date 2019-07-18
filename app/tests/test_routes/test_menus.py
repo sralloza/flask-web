9b05ffc2f96a802874106c64ab8908785a4f6538
sralloza <sralloza@gmail.com>
2019-07-18 18:14:47 +0200

    Organize tests

---
+++ app/tests/test_routes/test_menus.py
@@ -0,0 +1,31 @@
+import pytest
+
+
+@pytest.mark.skip
+def test_menus():
+    pass
+
+
+@pytest.mark.skip
+def test_redirect_menus():
+    pass
+
+
+@pytest.mark.skip
+def test_menus_reload():
+    pass
+
+
+@pytest.mark.skip
+def test_today_redirect():
+    pass
+
+
+@pytest.mark.skip
+def test_today():
+    pass
+
+
+@pytest.mark.skip
+def test_api_menus():
+    pass
