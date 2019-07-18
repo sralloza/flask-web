d389978d5daeb17112c21a8cf7f8ae3af475496c
sralloza <sralloza@gmail.com>
2019-07-18 17:36:47 +0200

    Fix utils.get_last_menus_page

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -2,28 +2,37 @@ import logging
 
 import requests
 from bs4 import BeautifulSoup as Soup
+from requests.exceptions import ConnectionError
 
 logger = logging.getLogger(__name__)
+
+PRINCIPAL_URL = 'https://www.residenciasantiago.es/menus-1/'
 
 
 class _Static:
     redirect_url = None
 
 
-def get_last_menus_page():
-    principal_url = 'https://www.residenciasantiago.es/menus-1/'
-
+def get_last_menus_page(retries=5):
     if _Static.redirect_url:
         return _Static.redirect_url
 
-    try:
-        response = requests.get(principal_url)
-        soup = Soup(response.text, 'html.parser')
-        container = soup.findAll('div', {'class': 'j-blog-meta'})
+    total_retries = retries
+    logger.debug('Getting last menus url')
 
-        redirect_url = container[0].a['href']
-        _Static.redirect_url = redirect_url
-        return redirect_url
-    except Exception:
-        logger.exception('Failed detecting this week\'s menus url')
-        return principal_url
+    while retries:
+        try:
+            response = requests.get(PRINCIPAL_URL)
+            soup = Soup(response.text, 'html.parser')
+            container = soup.findAll('div', {'class': 'j-blog-meta'})
+            urls = [x.a['href'] for x in container]
+            return urls[0]
+        except ConnectionError:
+            retries -= 1
+            logger.warning('Connection error downloading principal url (%r) (%d retries left)',
+                           PRINCIPAL_URL, retries)
+
+    logger.critical(
+        'Fatal connection error downloading principal url (%r) (%d retries)',
+        PRINCIPAL_URL, total_retries)
+    return PRINCIPAL_URL
