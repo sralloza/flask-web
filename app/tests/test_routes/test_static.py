d42dcac76d7c90d757b2c61cf4296a42c3a5df0c
sralloza <sralloza@gmail.com>
2019-07-18 17:43:24 +0200

    Test requests to static files

---
+++ app/tests/test_routes/test_static.py
@@ -0,0 +1,21 @@
+from flask import url_for
+
+
+class TestStaticFiles:
+    class TestCss:
+        def test_menus(self, client):
+            rv = client.get(url_for('static', filename='css/menus.css'))
+            assert rv.status_code == 200
+            assert rv.headers['Content-Type'] == 'text/css; charset=utf-8'
+
+    class TestImages:
+        def test_favicon(self, client):
+            rv = client.get(url_for('static', filename='images/favicon.png'))
+            assert rv.status_code == 200
+            assert rv.headers['Content-Type'] == 'image/png'
+
+    class TestJs:
+        def test_beta_datatable_menus(self, client):
+            rv = client.get(url_for('static', filename='js/beta-datatable-menus.js'))
+            assert rv.status_code == 200
+            assert rv.headers['Content-Type'] == 'text/plain; charset=utf-8'
