74343c365264b9c426d47a35a248952693f35d38
sralloza <sralloza@gmail.com>
2019-07-18 17:36:11 +0200

    Move SERVER_NAME config to TestingConfig

--- app/config.py
+++ app/config.py
@@ -5,7 +5,6 @@ class Config(object):
     LAST_URL = 'https://www.residenciasantiago.es/2019/03/04/semana-del-5-al-11-de-marzo-2019/'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///../flask.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SERVER_NAME = 'menus.sralloza.es'
 
 
 class TestingConfig(Config):
@@ -13,3 +12,4 @@ class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///flask_tmp.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SERVER_NAME = 'menus.sralloza.es'
