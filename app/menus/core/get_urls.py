f6f3e1c27739687c963e32f5de57dc4045ac59a2
sralloza <sralloza@gmail.com>
2019-07-18 15:31:04 +0200

    Improve get_menus_urls based on tests

--- app/menus/core/get_urls.py
+++ app/menus/core/get_urls.py
@@ -5,18 +5,20 @@ from bs4 import BeautifulSoup as Soup
 from requests.exceptions import ConnectionError
 
 logger = logging.getLogger(__name__)
+PRINCIPAL_URL = 'https://www.residenciasantiago.es/menus-1/'
 
 
 def get_menus_urls(principal_url=None, retries=5, index_path=None):
+    total_retries = retries
     logger.debug('Getting menus urls')
     if not principal_url:
-        principal_url = 'https://www.residenciasantiago.es/menus-1/'
+        principal_url = PRINCIPAL_URL
 
     while retries:
         try:
             if not index_path:
                 response = requests.get(principal_url)
-                soup = Soup(response.content, 'html.parser')
+                soup = Soup(response.text, 'html.parser')
             else:
                 with open(index_path, 'rb') as fh:
                     soup = Soup(fh.read(), 'html.parser')
@@ -24,10 +26,11 @@ def get_menus_urls(principal_url=None, retries=5, index_path=None):
             urls = [x.a['href'] for x in container]
             return urls
         except ConnectionError:
-            logger.warning('Connection error downloading principal url (%r)', principal_url)
             retries -= 1
+            logger.warning('Connection error downloading principal url (%r) (%d retries left)',
+                           principal_url, retries)
 
     logger.critical(
         'Fatal connection error downloading principal url (%r). No urls found (%d retries)',
-        principal_url, retries)
+        principal_url, total_retries)
     return []
