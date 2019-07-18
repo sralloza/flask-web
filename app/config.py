d9e19d579de9efc6951a4d0a7f6fc15fbfd008a5
sralloza <sralloza@gmail.com>
2019-07-18 15:34:22 +0200

    Change servername in config

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ class Config(object):
     LAST_URL = 'https://www.residenciasantiago.es/2019/03/04/semana-del-5-al-11-de-marzo-2019/'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///../flask.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SERVER_NAME = 'menus.sralloza.es'
 
 
 class TestingConfig(Config):
@@ -12,4 +13,3 @@ class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///flask_tmp.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SERVER_NAME = 'sralloza.sytes.net'
