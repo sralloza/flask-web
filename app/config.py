46dc6a2ab970e57d4dffe5af3953d2ae85cd7b47
sralloza <sralloza@gmail.com>
2019-08-14 17:39:09 +0200

    Add DATABASE_PATH config

--- app/config.py
+++ app/config.py
@@ -4,7 +4,8 @@ from pathlib import Path
 class Config(object):
     ROOT_PATH = Path(__file__).parent
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + ROOT_PATH.parent.joinpath('flask.db').as_posix()
+    DATABASE_PATH = ROOT_PATH.parent.joinpath('flask.db').as_posix()
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
