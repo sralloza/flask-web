c4d335d38103177e6d85da7b6650114866b60c4c
sralloza <sralloza@gmail.com>
2019-09-09 10:25:21 +0200

    Finish html parser

--- app/menus/core/parser/html_parser.py
+++ app/menus/core/parser/html_parser.py
@@ -6,73 +6,22 @@ from typing import Union, Iterable, List
 import requests
 from bs4 import BeautifulSoup as Soup
 
-from app.menus.core.parser import BaseParser, BaseWorker
 from app.menus.core.structure import DailyMenu, Index
-from app.menus.core.utils import get_menus_urls, Patterns, filter_data, has_day
+from app.menus.core.utils import Patterns, filter_data, has_day
+from .abc import BaseParser, BaseWorker
 
 logger = logging.getLogger(__name__)
 M = Union[DailyMenu, Iterable[DailyMenu]]
+ML = List[DailyMenu]
 S = List[str]
 
 
 class HtmlParser(BaseParser):
     """Represents a controller of a list of menus."""
+    _lock = Lock()
 
-    def __init__(self, dmm):
-        super().__init__(dmm)
-        self.menus = []
-        self._lock = Lock()
-
-    def save_menu(self, menus: M):
-        """Adds menus to the database.
-
-        Args:
-            menus: menus to add.
-
-        """
-
-        with self._lock:
-            if isinstance(menus, DailyMenu):
-                menus = [menus, ]
-
-            existing_dates = [x.date for x in self.menus]
-            new_menus = []
-            for menu in menus:
-                if menu.date not in existing_dates:
-                    new_menus.append(menu)
-
-            self.menus += new_menus
-
-    @classmethod
-    def load(cls, dmm, urls: S=None):
-        """Loads menus using HtmlParser.
-
-        Args:
-            dmm (DailyMenusManager): DailyMenusManager to save the menus.
-            urls (S): List of urls to parse. If None, urls will be determined
-                by get_menus_url().
-
-        """
-
-        self = HtmlParser(dmm=dmm)
-        logger.debug('Loading from menus urls')
-
-        if not urls:
-            urls = get_menus_urls()
-
-        threads = []
-        for url in urls:
-            t = HtmlParserWorker(url, self)
-            t.start()
-            threads.append(t)
-
-        for thread in threads:
-            thread.join()
-
-        logger.debug('HtmlParser finished, copying menus to DailyMenusManager')
-        self.dmm.add_to_menus(self.menus)
-
-    def process_url(self, url: str, retries=5):
+    @staticmethod
+    def process_url(dmm, url: str, retries=5):
         """Processes url in search from menus."""
         logger.debug('Processing url %r', url)
         r = None
@@ -95,13 +44,13 @@ class HtmlParser(BaseParser):
 
         texts = filter_data(texts)
         menus = [DailyMenu.from_datetime(x) for x in texts if has_day(x)]
+        assert menus
 
-        self.save_menu(menus)
-        self._process_texts(texts)
+        HtmlParser._process_texts(texts, menus)
+        dmm.add_to_menus(menus)
 
-        return self
-
-    def _process_texts(self, texts):
+    @staticmethod
+    def _process_texts(texts: S, menus: ML):
         """Processes texts retrieved from url."""
         logger.debug('Processing texts')
         index = Index()
@@ -109,7 +58,7 @@ class HtmlParser(BaseParser):
             text = text.replace('_', ' ').lower()
             if Patterns.day_pattern.search(text) is not None:
                 if index.commit():
-                    self._update_menu(index)
+                    HtmlParser._update_menu(index, menus)
 
                 index.reset()
                 search = Patterns.day_pattern.search(text)
@@ -144,15 +93,18 @@ class HtmlParser(BaseParser):
                     index.decide(text)
 
         if index.commit():
-            self._update_menu(index)
+            HtmlParser._update_menu(index, menus)
 
-    def _update_menu(self, index: Index):
+        return menus
+
+    @staticmethod
+    def _update_menu(index: Index, menus: ML):
         logger.debug('Updating menu')
-        with self._lock:
-            for i, menu in enumerate(self.menus):
-                if self.menus[i].date == index.date:
+        with HtmlParser._lock:
+            for i, menu in enumerate(menus):
+                if menus[i].date == index.date:
                     if index.is_combinated:
-                        self.menus[i].set_combined(index.meal_combined)
+                        menus[i].set_combined(index.meal_combined)
 
                     index_info = index.to_dict()
                     index_info.pop('day', None)
@@ -161,7 +113,7 @@ class HtmlParser(BaseParser):
                     index_info.pop('date', None)
                     index_info.pop('weekday', None)
 
-                    self.menus[i].update(**index_info)
+                    menus[i].update(**index_info)
                     break
 
 
