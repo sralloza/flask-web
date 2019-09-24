a7b19c7908d472cc4e99666a8ed4049648b37175
sralloza <sralloza@gmail.com>
2019-09-24 12:20:13 +0200

    Avoid downloading menus web when testing status codes

--- app/tests/test_menus/test_menus_routes.py
+++ app/tests/test_menus/test_menus_routes.py
@@ -133,7 +133,9 @@ def test_today_reload(dmm_mock, client, menu_mock):
     assert menu_mock.to_database.call_count == 7
 
 
-def test_today(client):
+@mock.patch('app.menus.core.daily_menus_manager.UpdateControl.should_update')
+def test_today(su_mock, client):
+    su_mock.return_value = False
     rv = client.get('/hoy')
     assert rv.status_code == 200
     assert b'menus.css' in rv.data
