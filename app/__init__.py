0b17128c9b89c99918472bb9a7ea6b3d5e2c9594
sralloza <sralloza@gmail.com>
2019-09-25 20:23:17 +0200

    Remove unused config options of create_app

--- app/__init__.py
+++ app/__init__.py
@@ -22,17 +22,10 @@ werkzeug_handler.setFormatter(logging.Formatter(fmt='%(asctime)s] %(levelname)s 
 werkzeug.addHandler(werkzeug_handler)
 
 
-def create_app(config_filename=None, config_object=None, settings_override=None):
+def create_app(config_object):
     flask_app = Flask(__name__)
 
-    if config_filename:
-        flask_app.config.from_pyfile(config_filename)
-    elif config_object:
-        flask_app.config.from_object(config_object)
-    elif settings_override:
-        flask_app.config.from_json(settings_override)
-    else:
-        raise RuntimeError('Select config source (file or object)')
+    flask_app.config.from_object(config_object)
 
     db.init_app(flask_app)
 
