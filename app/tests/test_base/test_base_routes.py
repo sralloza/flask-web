b73abac675442f3c2c698ba64c115404a9710b22
sralloza <sralloza@gmail.com>
2019-10-01 18:35:48 +0200

    Autospec mocks of tests of base routes

--- app/tests/test_base/test_base_routes.py
+++ app/tests/test_base/test_base_routes.py
@@ -6,60 +6,67 @@ from werkzeug.wrappers import BaseResponse
 
 def test_user_agent_filter(app):
     client = FlaskClient(app, BaseResponse)
-    del client.environ_base['HTTP_USER_AGENT']
+    del client.environ_base["HTTP_USER_AGENT"]
 
-    assert client.get('/').status_code == 401
-    assert client.get('/', headers={'User-Agent': 'python-requests/2.21.0'}).status_code == 401
-    assert client.get('/', headers={'User-Agent': '-'}).status_code == 401
-    assert client.get('/', headers={'User-Agent': 'Rift/2.0'}).status_code == 401
+    assert client.get("/").status_code == 401
+    assert (
+        client.get("/", headers={"User-Agent": "python-requests/2.21.0"}).status_code
+        == 401
+    )
+    assert client.get("/", headers={"User-Agent": "-"}).status_code == 401
+    assert client.get("/", headers={"User-Agent": "Rift/2.0"}).status_code == 401
 
 
 def test_redirect_favicon(client):
-    rv = client.get('/favicon.ico')
+    rv = client.get("/favicon.ico")
     assert rv.status_code == 301
-    assert rv.location == 'http://menus.sralloza.es/static/images/favicon.png'
+    assert rv.location == "http://menus.sralloza.es/static/images/favicon.png"
 
 
 def test_index(client):
-    rv = client.get('/')
+    rv = client.get("/")
     assert 200 <= rv.status_code <= 399
 
 
 def test_redirect_index(client):
-    rv = client.get('/')
+    rv = client.get("/")
     assert rv.status_code == 302
-    assert rv.location == 'http://menus.sralloza.es/hoy'
+    assert rv.location == "http://menus.sralloza.es/hoy"
 
 
 def test_redirect_source(client):
-    rv = client.get('/s')
+    rv = client.get("/s")
     assert rv.status_code == 301
-    assert rv.location == 'http://menus.sralloza.es/source'
+    assert rv.location == "http://menus.sralloza.es/source"
 
 
-@mock.patch('app.base.routes.get_last_menus_page', return_value='http://example.com')
+@mock.patch(
+    "app.base.routes.get_last_menus_page",
+    return_value="http://example.com",
+    autospec=True,
+)
 def test_source(get_last_menus_page_mock, client):
-    rv = client.get('/source')
+    rv = client.get("/source")
     assert rv.status_code == 302
-    assert rv.location == 'http://example.com'
+    assert rv.location == "http://example.com"
 
     get_last_menus_page_mock.assert_called_once_with()
 
 
 def test_feedback(client):
-    rv = client.get('/feedback')
+    rv = client.get("/feedback")
     assert rv.status_code == 200
-    assert b'<h1>Send feedback to sralloza@gmail.com</h1>' in rv.data
+    assert b"<h1>Send feedback to sralloza@gmail.com</h1>" in rv.data
 
 
 def test_redirect_aemet(client):
-    rv = client.get('/a')
+    rv = client.get("/a")
     assert rv.status_code == 301
-    assert rv.location == 'http://menus.sralloza.es/aemet'
+    assert rv.location == "http://menus.sralloza.es/aemet"
 
 
 def test_aemet(client):
-    aemet = 'http://www.aemet.es/es/eltiempo/prediccion/municipios/horas/tabla/valladolid-id47186'
-    rv = client.get('/aemet')
+    aemet = "http://www.aemet.es/es/eltiempo/prediccion/municipios/horas/tabla/valladolid-id47186"
+    rv = client.get("/aemet")
     assert rv.status_code == 301
     assert rv.location == aemet
