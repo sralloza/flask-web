4405cab797f1bedc54d5d7b2b49d68379f845dc1
sralloza <sralloza@gmail.com>
2019-10-07 12:48:10 +0200

    config.DATABASE_PATH is instance of pathlib.Path

--- app/config.py
+++ app/config.py
@@ -3,15 +3,15 @@ from pathlib import Path
 
 
 class Config(object):
-    ROOT_PATH = Path(__file__).parent
-    TEST_DATA_PATH = ROOT_PATH / 'tests' / 'data'
-    TESTING = False
-    DATABASE_PATH = ROOT_PATH.parent.joinpath('flask.db').as_posix()
-    IS_LINUX = platform.system() == 'Linux'
-    BASE_RESIDENCE_URL = 'https://www.residenciasantiago.es'
+    ROOT_PATH: Path = Path(__file__).parent
+    TEST_DATA_PATH: Path = ROOT_PATH / "tests" / "data"
+    TESTING: bool = False
+    DATABASE_PATH: Path = ROOT_PATH.parent.joinpath("flask.db")
+    IS_LINUX: bool = platform.system() == "Linux"
+    BASE_RESIDENCE_URL: str = "https://www.residenciasantiago.es"
 
 
 class TestingConfig(Config):
-    TESTING = True
-    DATABASE_PATH = Path(Config.DATABASE_PATH).with_name('test-flask.db').as_posix()
-    SERVER_NAME = 'menus.sralloza.es'
+    TESTING: bool = True
+    DATABASE_PATH: Path = Path(Config.DATABASE_PATH).with_name("test-flask.db")
+    SERVER_NAME: str = "menus.sralloza.es"
