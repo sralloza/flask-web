3c8b4be4a4e44624a1ffc808082b58b9fc1314df
sralloza <sralloza@gmail.com>
2019-11-20 22:59:45 +0100

    Update version and move VERSION to separate file

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
-from setuptools import setup, find_packages
+from pathlib import Path
 
-setup(name="app", pagackes=find_packages())
+from setuptools import find_packages, setup
+
+
+def get_version():
+    path = Path(__file__).with_name("VERSION")
+    return path.read_text().strip()
+
+
+setup(version=get_version(), name="app", pagackes=find_packages())
