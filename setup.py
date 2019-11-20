47a47affbe6e321f261af47fd6522e83469d4ef6
sralloza <sralloza@gmail.com>
2019-11-20 23:06:18 +0100

    Add requirements in setup

--- setup.py
+++ setup.py
@@ -8,4 +8,15 @@ def get_version():
     return path.read_text().strip()
 
 
-setup(version=get_version(), name="app", pagackes=find_packages())
+def get_requirements():
+    path = Path(__file__).with_name("requirements.txt")
+    return [x.strip() for x in path.read_text().split("\n") if x.strip()]
+
+
+setup(
+    version=get_version(),
+    name="app",
+    pagackes=find_packages(),
+    requirements=get_requirements(),
+    # package_data={"": [""]},
+)
