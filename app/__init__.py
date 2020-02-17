61c3e43ecc02e61a25691e5da4f195db45577b34
sralloza <sralloza@gmail.com>
2020-02-17 16:29:04 +0100

    Change logging format to universal format

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ from .menus import menus_blueprint
 logging.basicConfig(
     filename=Path(__file__).parent.parent / "flask-app.log",
     level=logging.DEBUG,
-    format="[%(asctime)s] %(levelname)s - %(name)s:%(lineno)s - %(message)s",
+    format = "[%(asctime)s] %(levelname)s - %(threadName)s.%(module)s:%(lineno)s - %(message)s",
 )
 
 werkzeug = logging.getLogger("werkzeug")
