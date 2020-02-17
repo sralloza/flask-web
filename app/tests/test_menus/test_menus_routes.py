3817002f6b42835a7c643b32cccc2d629863655d
sralloza <sralloza@gmail.com>
2020-02-17 16:57:12 +0100

    Update test of /hoy endpoint
    get_last_menus_page is no longer called

--- app/tests/test_menus/test_menus_routes.py
+++ app/tests/test_menus/test_menus_routes.py
@@ -137,13 +137,8 @@ def test_today_reload(dmm_mock, client, menu_mock):
     menu_mock.to_database.assert_not_called()
 
 
-@mock.patch(
-    "app.menus.routes.get_last_menus_page",
-    return_value="http://example.com",
-    autospec=True,
-)
 @mock.patch("app.menus.core.daily_menus_manager.UpdateControl.should_update")
-def test_today(su_mock, glmp_mock, client, reset_database):
+def test_today(su_mock, client, reset_database):
     su_mock.return_value = False
     rv = client.get("/hoy")
     assert rv.status_code == 200
@@ -151,7 +146,6 @@ def test_today(su_mock, glmp_mock, client, reset_database):
     assert b"loader.css" in rv.data
     assert b"getElementById" in rv.data
     assert b"today-js.js" in rv.data
-    assert b"http://example.com" in rv.data
 
 
 class MenuApiDataCodes(Enum):
