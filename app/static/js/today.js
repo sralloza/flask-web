8cc679ea06fdef3d448d949dfbd4a5ed39bce264
sralloza <sralloza@gmail.com>
2020-02-20 21:39:42 +0100

    Add letter H as keybinding to jump to today

--- app/static/js/today.js
+++ app/static/js/today.js
@@ -282,7 +282,7 @@ function detectArrows(e) {
 };
 
 function detectKey(e) {
-    // Detects letters J, K and T
+    // Detects letters J, K, H and T
     switch (e.keyCode) {
         case 106:
         case 74:
@@ -296,15 +296,23 @@ function detectKey(e) {
             console.log("Letter K pressed")
             clickNext();
             break;
+        case 104:
+        case 72:
+            // Letter H
+            console.log("Letter T pressed");
+            jumpToToday();
+            break;
         case 116:
         case 84:
             // Letter T
             console.log("Letter T pressed");
-            today = new Date();
-            // console.log(today.getDate());
-            // dateViewed.setDate(today.getDate());
-            dateViewed = today;
-            updateInterface();
+            jumpToToday()
             break;
     }
+}
+
+function jumpToToday() {
+    today = new Date();
+    dateViewed = today;
+    updateInterface();
 }
