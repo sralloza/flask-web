a24a98a83ddbe559f68ca4e2d30a93826cf21d26
sralloza <sralloza@gmail.com>
2020-03-01 21:14:11 +0100

    Add individual tests for urls.json

--- app/tests/test_menus/test_core/test_parser/test_urls_json.py
+++ app/tests/test_menus/test_core/test_parser/test_urls_json.py
@@ -4,14 +4,18 @@ from app.tests.data.data import ParserPaths
 
 
 @pytest.fixture(scope="class")
-def urls_json():
+def urls_json_sep():
     json_data = ParserPaths.urls_dict.value
 
+    yield json_data
+
+
+@pytest.fixture(scope="class")
+def urls_json(urls_json_sep):
     hole_data = {}
-    for _, dict1 in json_data.items():
+    for _, dict1 in urls_json_sep.items():
         for key2, value in dict1.items():
             hole_data[key2] = value
-
     yield hole_data
 
 
@@ -20,17 +24,17 @@ def name(path):
 
 
 class TestJsonUrls:
-    def test_pdfs(self, urls_json):
+    def test_pdfs(self, urls_json_sep):
         for url in ParserPaths.pdf.value:
-            assert name(url) in urls_json
+            assert name(url) in urls_json_sep["pdf"]
 
-    def test_photos(self, urls_json):
+    def test_photos(self, urls_json_sep):
         for url in ParserPaths.photos.value:
-            assert name(url) in urls_json
+            assert name(url) in urls_json_sep["photos"]
 
-    def test_html(self, urls_json):
+    def test_html(self, urls_json_sep):
         for url in ParserPaths.html.value:
-            assert name(url) in urls_json
+            assert name(url) in urls_json_sep["html"]
 
     def test_not_pdf(self, urls_json):
         for url in ParserPaths.not_pdf.value:
