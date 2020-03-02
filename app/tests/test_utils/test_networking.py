7489491688b0c5d49717082538355f7f63928fab
sralloza <sralloza@gmail.com>
2020-03-02 11:41:11 +0100

    Add tests for Downloader

--- app/tests/test_utils/test_networking.py
+++ app/tests/test_utils/test_networking.py
@@ -1,7 +1,21 @@
+from collections import Counter
+from unittest import mock
+
 import pytest
+from requests import exceptions as req_exc
 
 from app.utils import MetaSingleton
-from app.utils.networking import Downloader
+from app.utils.exceptions import DownloaderError
+from app.utils.networking import USER_AGENT, Downloader
+
+REQUESTS_EXCEPTIONS = (
+    req_exc.URLRequired,
+    req_exc.TooManyRedirects,
+    req_exc.HTTPError,
+    req_exc.ConnectionError,
+    req_exc.ConnectTimeout,
+    req_exc.ReadTimeout,
+)
 
 
 def test_use_singleton():
@@ -16,7 +30,80 @@ def test_use_singleton():
     assert c is a
 
 
-@pytest.mark.xfail
+@mock.patch("requests.Session.request")
 class TestDownloader:
-    def test_something(self):
-        assert 0, "Not implemented"
+    @pytest.fixture(autouse=True)
+    def reset_singleton(self):
+        Downloader._instance = None
+
+    def test_init_declaration(self, *args):
+        downloader = Downloader()
+        assert hasattr(downloader, "logger")
+        assert hasattr(downloader, "retries")
+
+        assert downloader.headers["User-Agent"] == USER_AGENT
+
+    def test_get(self, request_mock, caplog):
+        downloader = Downloader()
+        downloader.get("some-url")
+        request_mock.assert_called_with("GET", "some-url", allow_redirects=True)
+        assert "GET 'some-url'" in caplog.text
+
+    def test_post(self, request_mock, caplog):
+        downloader = Downloader()
+        downloader.post("some-url", data="data", json="json")
+        request_mock.assert_called_with("POST", "some-url", data="data", json="json")
+        assert "POST 'some-url'" in caplog.text
+
+    def test_delete(self, request_mock, caplog):
+        downloader = Downloader()
+        downloader.delete("some-url")
+        request_mock.assert_called_with("DELETE", "some-url")
+        assert "DELETE 'some-url'" in caplog.text
+
+    def test_put(self, request_mock, caplog):
+        downloader = Downloader()
+        downloader.put("some-url", data="data")
+        request_mock.assert_called_with("PUT", "some-url", data="data")
+        assert "PUT 'some-url'" in caplog.text
+
+    def test_head(self, request_mock, caplog):
+        downloader = Downloader()
+        downloader.head("some-url")
+        # Default allow_redirects for HEAD is false
+        request_mock.assert_called_with("HEAD", "some-url", allow_redirects=False)
+        assert "HEAD 'some-url'" in caplog.text
+
+    @pytest.mark.parametrize("error_class", REQUESTS_EXCEPTIONS)
+    @pytest.mark.parametrize("retries", range(1, 5))
+    @pytest.mark.parametrize("nerrors", range(5))
+    def test_retry_control(self, request_mock, caplog, nerrors, retries, error_class):
+        request_mock.side_effect = [error_class] * nerrors + ["response"]
+        caplog.set_level(10)
+
+        downloader = Downloader(retries=retries)
+
+        if nerrors > downloader.retries:
+            with pytest.raises(DownloaderError):
+                downloader.post("some-url", "data", "json")
+        else:
+            downloader.post("some-url", "data", "json")
+
+        request_mock.assert_called()
+        logger_method_count = Counter([x.levelname for x in caplog.records])
+
+        if nerrors:
+            assert "%s in POST" % error_class.__name__ in caplog.text
+
+        if nerrors > downloader.retries:
+            assert request_mock.call_count == downloader.retries + 1
+            assert logger_method_count["DEBUG"] == 1
+            assert logger_method_count["INFO"] == 0
+            assert logger_method_count["WARNING"] == downloader.retries + 1
+            assert logger_method_count["CRITICAL"] == 1
+        else:
+            assert request_mock.call_count == nerrors + 1
+            assert logger_method_count["DEBUG"] == 1
+            assert logger_method_count["INFO"] == 0
+            assert logger_method_count["WARNING"] == nerrors
+            assert logger_method_count["CRITICAL"] == 0
--- app/utils/networking.py
+++ app/utils/networking.py
@@ -5,42 +5,37 @@ import requests
 
 from .exceptions import DownloaderError
 from . import MetaSingleton
+
 logger = logging.getLogger(__name__)
+
+USER_AGENT = (
+    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+    " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
+)
 
 
 class Downloader(requests.Session, metaclass=MetaSingleton):
     """Downloader with retries control."""
 
-    def __init__(self, silenced=False, retries=5):
+    def __init__(self, retries=5):
         self.logger = logging.getLogger(__name__)
         self.retries = retries
 
-        if silenced is True:
-            self.logger.setLevel(logging.CRITICAL)
-
         super().__init__()
-        self.headers.update(
-            {
-                "User-Agent": "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
-            }
-        )
+        self.headers.update({"User-Agent": USER_AGENT})
 
     def request(self, method, url, **kwargs):
         self.logger.debug("%s %r", method, url)
         retries = self.retries
 
-        while retries > 0:
+        while retries >= 0:
             try:
                 return super().request(method, url, **kwargs)
-            except requests.exceptions.ConnectionError:
+            except requests.exceptions.RequestException as exc:
                 retries -= 1
                 self.logger.warning(
-                    "Connection error in %s, retries=%s", method, retries
+                    "%s in %s, retries=%s", type(exc).__name__, method, retries
                 )
-            except requests.exceptions.ReadTimeout:
-                retries -= 1
-                self.logger.warning("Timeout error in %s, retries=%s", method, retries)
 
         self.logger.critical("Download error in %s %r", method, url)
         raise DownloaderError("max retries failed.")
