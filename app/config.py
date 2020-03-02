cf506b6e9da5364122f64116cf060ee6bbf319e8
sralloza <sralloza@gmail.com>
2020-03-02 18:37:56 +0100

    Add docs in config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,13 @@
+"""This module contains the config for the application.
+There is a main class (`Config`), and the rest are subclasses of it.
+The second class is `TestingConfig`, desinged to be used during tests.
+"""
 import platform
 from pathlib import Path
 
 
-class Config(object):
+class Config:
+    """General config class."""
     ROOT_PATH: Path = Path(__file__).parent
     TEST_DATA_PATH: Path = ROOT_PATH / "tests" / "data"
     TESTING: bool = False
@@ -14,6 +19,7 @@ class Config(object):
 
 
 class TestingConfig(Config):
+    """Config class to use during tests."""
     TESTING: bool = True
     DATABASE_PATH: Path = Path(Config.DATABASE_PATH).with_name("test-flask.db")
     SERVER_NAME: str = "menus.sralloza.es"
