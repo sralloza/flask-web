4f5afd32bf759908a7704772bac19d41757c9ad0
sralloza <sralloza@gmail.com>
2020-03-02 23:01:50 +0100

    Add docs in menus.core.parser.abc

--- app/menus/core/parser/abc.py
+++ app/menus/core/parser/abc.py
@@ -1,3 +1,4 @@
+"""Abstract bases for Parsers."""
 import logging
 from abc import ABC, abstractmethod
 from typing import List
@@ -7,7 +8,16 @@ logger = logging.getLogger(__name__)
 
 
 class BaseParser(ABC):
+    """Abstract class for Parsers."""
     @classmethod
     @abstractmethod
     def process_text(cls, dmm, text: str, url: str):
+        """Process the text previously downloaded from url generating
+        a set of instances of DailyMenu, which links to dmm.
+
+        Args:
+            dmm (DailyMenusManager): DMM instance to link the menus to.
+            text (str): texts downloaded from url.
+            url (str): url from which the menus were downloaded.
+        """
         raise NotImplementedError
