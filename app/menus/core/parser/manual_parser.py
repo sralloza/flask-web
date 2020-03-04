8abb8fc03d3a20e24e741a7bc966c1bdfa52e6ba
sralloza <sralloza@gmail.com>
2020-03-04 15:28:11 +0100

    Add docs in app.menus.core.parser.manual_parser

--- app/menus/core/parser/manual_parser.py
+++ app/menus/core/parser/manual_parser.py
@@ -1,3 +1,4 @@
+"""Parser for non-html data."""
 from bs4 import BeautifulSoup
 
 from app.menus.core.exceptions import ParserError
@@ -6,6 +7,11 @@ from app.menus.core.parser import BaseParser
 
 class ManualParser(BaseParser):
     def __init__(self, soup):
+        """Parser for non-html data.
+
+        Args:
+            soup (bs4.BeautifulSoup): text parsed by `bs4.BeautifulSoup`.
+        """
         self.soup = soup
 
     @classmethod
@@ -23,6 +29,11 @@ class ManualParser(BaseParser):
         raise ParserError
 
     def detect_if_pdf(self):
+        """Detects if menus data is in pdf format.
+
+        Returns:
+            bool: True if menus data is in pdf format, False otherwise.
+        """
         try:
             pdf = self.soup.find("div", class_="cc-m-download-file-link").a["href"]
             return pdf is not None
@@ -30,6 +41,11 @@ class ManualParser(BaseParser):
             return False
 
     def detect_if_photo(self):
+        """Detects if menus data is in photo format.
+
+        Returns:
+            bool: True if menus data is in photo format, False otherwise.
+        """
         container = self.soup.find("div", class_="post j-blog-content")
         photo = container.find("img", alt="")
         return photo is not None
